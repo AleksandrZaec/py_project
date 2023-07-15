8f06c6579279f86399963d313c239748835bd940
AleksandrZaec <zaecam95@mail.ru>
2023-07-16 01:31:01 +0300

    Добавил функцию вычитания

--- funcs.py
+++ funcs.py
@@ -1,9 +1,10 @@
-print("Привет, напиши два числа")
-x = int(input())
-y = int(input())
-
 def add_numbers(x,y):
    sum = x + y
    return sum
 
-print(add_numbers(x,y))
+def sub_numbers(x,y):
+   sub = x - y
+   return sub
+
+
+
